da0866e7953fb4e3cfb8740de5e042790cfdffa8
Jul-Me <memai.julian@gmail.com>
2022-01-14 15:18:30 +0000

    data successfuly returned

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
 import Header from '../Header/Header';
 import './App.css';
 
 function App() {
+  const [memes, setMemes] = useState([]);
+
+  const fetchData = () => {
+    return fetch('https://api.imgflip.com/get_memes')
+      .then((response) => response.json())
+      .then((data) => setMemes(data.data.memes));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(`state`, memes);
+  }, [memes]);
+
   return (
     <div className="app">
       <Header />
