97f1ab39d571bad01c1e3c23bce34c08bd3f3300
Jul-Me <memai.julian@gmail.com>
2022-01-14 16:04:17 +0000

    useReducer hook to update the state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useReducer } from 'react';
 import Header from '../Header/Header';
 import './App.css';
 
+const initialState = { allMemes: [], displayMemes: [] };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'getAllMemes':
+      return {
+        ...state,
+        allMemes: [...state.allMemes, ...action.payload],
+      };
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [memes, setMemes] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const fetchData = () => {
     return fetch('https://api.imgflip.com/get_memes')
       .then((response) => response.json())
-      .then((data) => setMemes(data.data.memes));
+      .then((data) =>
+        dispatch({ type: 'getAllMemes', payload: data.data.memes })
+      );
   };
 
   useEffect(() => {
@@ -16,8 +32,8 @@ function App() {
   }, []);
 
   useEffect(() => {
-    console.log(`state`, memes);
-  }, [memes]);
+    console.log(`state`, state.allMemes);
+  }, [state.allMemes]);
 
   return (
     <div className="app">
